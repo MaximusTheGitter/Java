62975c8e93691e2a50bb74fe9a7301551fe3bc75
MaximusTheGitter <nreiziger@roc-dev.com>
2018-09-24 14:22:02 +0200

    Java opdrachten

---
+++ src/H11/opdracht_1.java
@@ -0,0 +1,21 @@
+package H11;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class opdracht_1 extends Applet {
+
+        public void init() {}
+
+        public void paint(Graphics g) {
+            int teller;
+            int x = 0;
+
+            for(teller = 0; teller < 10; teller++) {
+                x += 20;
+                g.drawLine( x , 10, x, 300);
+                g.drawString("" + teller, x,  10 );
+            }
+        }
+    }
+
